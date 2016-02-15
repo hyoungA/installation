3be265454708a2843e88c7f5e2d68f8bf9adbed3
hyoungA <h84.kim@gmail.com>
2016-02-15 23:02:49 +0900

    tested

--- src/main/java/installation/Installation.java
+++ src/main/java/installation/Installation.java
@@ -1,9 +1,11 @@
 package installation;
 
 import java.io.BufferedWriter;
-import java.io.File;
-import java.io.FileOutputStream;
+import java.io.FileWriter;
 import java.io.OutputStreamWriter;
+import java.nio.file.FileSystemAlreadyExistsException;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.apache.velocity.Template;
 import org.apache.velocity.VelocityContext;
@@ -14,22 +16,61 @@ public class Installation {
 		Velocity.init("src/main/resources/velocity.properties");
 		
 		VelocityContext context = new VelocityContext();
-		Template template = null; 
+		List<Template> templateList = new ArrayList<Template>();
+		
+		Template app_batch_properties_template = null;
+		Template app_jdbc_properties_template = null;
+		
+		// outfile writer 
+    	FileWriter f = null;
 
 		// 로직과 연결되어 사용될 템플릿 파일을 선택
-		template = Velocity.getTemplate("src/main/resources/App/target/classes/batch.properties.vm");
-		BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(System.out)); 
+		app_batch_properties_template = Velocity.getTemplate("resources/App/target/classes/batch.properties.vm");
+		app_jdbc_properties_template = Velocity.getTemplate("resources/App/target/classes/jdbc.properties.vm");
 
-		// 값 입력
-        context.put("LOG_ROOT", "${LOG_ROOT}");
-        context.put("DB_TYPE", "${DB_TYPE}");		
+
+		templateList.add(app_batch_properties_template);
+		templateList.add(app_jdbc_properties_template);
+		
+		// properties 값 입력
+        context.put("BATCH_LOG_DIRECTORY", Velocity.getProperty("APP_BATCH_LOG_DIRECTORY"));
+        context.put("BATCH_DATABASE_TYPE", Velocity.getProperty("APP_BATCH_DATABASE_TYPE"));
+        context.put("FRW_JDBC_DRIVERCLASSNAME", Velocity.getProperty("APP_FRW_JDBC_DRIVERCLASSNAME"));
         
+        // 템플릿별 파일 생성
+        for(Template t : templateList){
+
+        	try {
+        		// 일단 경로는 현재 폴더의 .vm 을 제거한 파일로 생성.
+            	f = new FileWriter(t.getName().substring(0, t.getName().length() - 3));
+			} catch (Exception e ) {
+    			e.printStackTrace();
+			}
+        	
+        	BufferedWriter writer = new BufferedWriter(f);
+//    		BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(System.out)); 
+        	
+        	// 템플릿과 컨텍스트를 렌더링해서 그 결과를 출력 
+    		if ( t != null)
+    		  t.merge(context, writer);
+
+    		// 결과 쓰기
+    		try {
+    			writer.flush(); 		
+    			writer.close();
+
+    		} catch (Exception e) {
+    			// TODO: handle exception
+    			e.printStackTrace();
+    		}
+        }
 //        String outFile= "src/main/java/sds/velocity/ClassTemplate.java";
 //        File f = new File(outFile);
 //        
 //        if(!f.exists()){
 //        	f.createNewFile();
 //        }
+        
 //        
 //        OutputStreamWriter osw = new OutputStreamWriter(new FileOutputStream(f), "UTF-8");
 //        
@@ -41,17 +82,6 @@ public class Installation {
 //        System.out.println("템플릿 생성 종료......");
         
 		
-		// 템플릿과 컨텍스트를 렌더링해서 그 결과를 출력 
-		if ( template != null) 
-		  template.merge(context, writer);
-
-		try {
-			writer.flush(); 		
-			writer.close();
-
-		} catch (Exception e) {
-			// TODO: handle exception
-			e.printStackTrace();
-		}
+		
 	}
 }
